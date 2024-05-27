9eb84106d49a36fd596f816f70fc5d7c67ef43a8
Rafael Crepaldi Candido <crepaldimkt@gmail.com>
2024-05-27 13:22:31 -0300

    003

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 from vosk import Model, KaldiRecognizer
 import os
 import pyaudio
+import pyttsx3
+import json
+import core
 
-model1 = Model("model")
-rec = KaldiRecognizer(model1, 16000)
+# Síntese de fala
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+# Reconhecimento de fala
+
+model = Model('model')
+rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
 stream.start_stream()
 
+# Loop do reconhecimento de fala
 while True:
-    data = stream.read(4000)
+    data = stream.read(2048)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
+        result = rec.Result()
+        result = json.loads(result)
 
-print(rec.FinalResult())
+        if result is not None:
+            text = result['text']
+
+            print(text)
+            if text == 'assistente que horas são' or text == 'assistente me diga as horas':
+                speak(core.SystemInfo.get_time())
+            
